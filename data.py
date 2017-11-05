Use fixed mean and std for feature normalization to save time

data.py
@@ -11,10 +11,23 @@ from sklearn.model_selection import train_test_split
 seed = 1
 
 
+def standardize(images, mean=None, std=None):
+    if mean is None:
+        # They are available from all images.
+        mean = [[[29.24429131, 29.24429131, 29.24429131]]]
+    if std is None:
+        # They are available from all images.
+        std = [[[69.8833313, 63.37436676, 61.38568878]]]
+    x = (images - np.array(mean)) / (np.array(std) + 1e-7)
+    return x
+
+
 def _create_datagen(images, masks, img_gen, mask_gen):
     img_iter = img_gen.flow(images, seed=seed)
     # only hair
-    mask_iter = mask_gen.flow(np.expand_dims(masks[:, :, :, 0], axis=4), seed=seed)
+    mask_iter = mask_gen.flow(np.expand_dims(masks[:, :, :, 0], axis=4),
+                              # use same seed to apply same augmentation with image
+                              seed=seed)
 
     def datagen():
         while True:
@@ -44,6 +57,7 @@ def load_data(img_file, mask_file):
         # vertical_flip=True,  # debug
         horizontal_flip=True,
     )
+    train_img_gen.fit(images)
     train_mask_gen = ImageDataGenerator(
         rescale=1. / 255,
         rotation_range=20,
@@ -52,7 +66,6 @@ def load_data(img_file, mask_file):
         # vertical_flip=True,  # debug
         horizontal_flip=True,
     )
-    train_img_gen.fit(images)  # fit with entire images
     train_gen = _create_datagen(X_train, Y_train,
                                 img_gen=train_img_gen,
                                 mask_gen=train_mask_gen)
@@ -140,7 +153,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--img_size',
         type=int,
-        default=128,
+        default=192,
     )
     args, _ = parser.parse_known_args()
 

pred.py
@@ -5,18 +5,17 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import load_model
-from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import CustomObjectScope
 from sklearn.model_selection import train_test_split
 
-from data import seed
+from data import seed, standardize
 from loss import np_dice_coef
 from nets.MobileUNet import custom_objects
 
-SAVED_MODEL1 = 'artifacts/128_1_025.h5'
-SAVED_MODEL2 = 'artifacts/128_075_025.h5'
+SAVED_MODEL1 = 'artifacts/224_1_1.h5'
+SAVED_MODEL2 = 'artifacts/224_1_1.h5'
 
-size = 128
+size = 224
 
 
 def main():
@@ -24,14 +23,10 @@ def main():
         model1 = load_model(SAVED_MODEL1)
         model2 = load_model(SAVED_MODEL2)
 
-    images = np.load('data/images-128.npy')
-    masks = np.load('data/masks-128.npy')
+    images = np.load('data/images-224.npy')
+    masks = np.load('data/masks-224.npy')
     # only hair
     masks = masks[:, :, :, 0].reshape(-1, size, size)
-
-    image_datagen = ImageDataGenerator(featurewise_center=True,
-                                       featurewise_std_normalization=True)
-    image_datagen.fit(images)
 
     _, images, _, masks = train_test_split(images,
                                            masks,
@@ -43,13 +38,12 @@ def main():
         batched2 = img.reshape(1, size, size, 3).astype(float)
 
         t1 = time.time()
-        # pred1 = model1.predict(batched / 255).reshape(size, size)
-        pred1 = model1.predict(image_datagen.standardize(batched1)).reshape(size, size)
+        pred1 = model1.predict(standardize(batched1)).reshape(size, size)
         elapsed = time.time() - t1
         print('elapsed1: ', elapsed)
 
         t1 = time.time()
-        pred2 = model2.predict(image_datagen.standardize(batched2)).reshape(size, size)
+        pred2 = model2.predict(standardize(batched2)).reshape(size, size)
         elapsed = time.time() - t1
         print('elapsed2: ', elapsed)
 