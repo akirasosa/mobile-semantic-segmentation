c7266cb209c8ee39053c26f4de83a10ed0e73c77
Akira Sosa <akirasosa@gmail.com>
2018-11-09 16:52:28 +0700

    fix output number

--- coreml_converter.py
+++ coreml_converter.py
@@ -8,32 +8,34 @@ from onnx_coreml import convert
 from nets.ImgWrapNet import ImgWrapNet
 
 # %%
+IMG_SIZE = 224
+
 TMP_ONNX = 'tmp/tmp.onnx'
 WEIGHT_PATH = 'outputs/train_unet/0-best.pth'
 ML_MODEL = re.sub('\.pth$', '.mlmodel', WEIGHT_PATH)
 
 # %%
 # Convert to ONNX once
-img = torch.randn(1, 3, 224, 224)
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = ImgWrapNet(torch.load(WEIGHT_PATH, map_location='cpu'))
 model.to(device)
 
-torch.onnx.export(model, img, TMP_ONNX)
+torch.onnx.export(model,
+                  torch.randn(1, 3, IMG_SIZE, IMG_SIZE),
+                  TMP_ONNX)
 
 # %%
 # Print out ONNX model to confirm the number of output layer
 onnx_model = onnx.load(TMP_ONNX)
-print('The model is:\n{}'.format(onnx_model))
+print(onnx_model)
 
 # %%
 # Convert ONNX to CoreML model
 model_file = open(TMP_ONNX, 'rb')
 model_proto = onnx_pb.ModelProto()
 model_proto.ParseFromString(model_file.read())
-# 590 is the identifier of output.
+# 595 is the identifier of output.
 coreml_model = convert(model_proto,
                        image_input_names=['0'],
-                       image_output_names=['590'])
+                       image_output_names=['595'])
 coreml_model.save(ML_MODEL)
